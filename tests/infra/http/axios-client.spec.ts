23ff0f50c9f03706c504946bb3d4c40c464ba684
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2024-07-23 15:21:50 -0300

    refactor: isolate sut creation

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -11,13 +11,28 @@ class AxiosHttpClient {
 }
 
 describe("AxiosHttpClient", () => {
+  let sut: AxiosHttpClient;
+  let fakeAxios: jest.Mocked<typeof axios>;
+  let url: string;
+  let params: object;
+
+  beforeAll(() => {
+    url = "any_url";
+    fakeAxios = axios as jest.Mocked<typeof axios>;
+    params = {
+      anyParams: "any_params",
+    };
+  });
+
+  beforeEach(() => {
+    sut = new AxiosHttpClient();
+  });
   describe("GET", () => {
     it("should call get with correct params", async () => {
-      const fakeAxios = axios as jest.Mocked<typeof axios>;
-      const sut = new AxiosHttpClient();
-      await sut.get({ url: "any_url", params: { anyParams: "any_params" } });
-      expect(fakeAxios.get).toHaveBeenCalledWith("any_url", {
-        params: { anyParams: "any_params" },
+      await sut.get({ url, params });
+
+      expect(fakeAxios.get).toHaveBeenCalledWith(url, {
+        params,
       });
       expect(fakeAxios.get).toHaveBeenCalledTimes(1);
     });
